b347146b92a426cfdc07a51afa59c350d4aa1ce9
Batyr Meredov <meredov_batyr@outlook.com>
2022-09-24 14:47:52 +0200

    added csharp project

---
+++ tsql-csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tsql_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
